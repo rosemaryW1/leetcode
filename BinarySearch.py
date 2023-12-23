61d457d9e2c96ebe816d7be523e07ab888eeebbe
rosemaryW1 <rosemary.wakaba@strathmore.edu>
2023-12-23 11:16:15 +0300

    binary search

---
+++ BinarySearch.py
@@ -0,0 +1,19 @@
+def binary_search(lst: object, item: object) -> object:
+    low = 0
+    high = len(lst)-1
+    while low <= high:
+      mid = (low+high)//2
+      guess = lst[mid]
+
+      if guess == item:
+          return mid
+      elif guess > item:
+          high = mid - 1
+      else:
+          low = mid + 1
+
+    return None
+
+
+
+
