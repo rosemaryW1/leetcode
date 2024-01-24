21db0a23ebaf83ed332a40892fa8e2ed2be7a344
rosemaryW1 <rosemary.wakaba@strathmore.edu>
2024-01-24 16:22:13 +0300

    InvertBinaryTree

---
+++ InvertBinaryTree.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+
+        #Swap the children
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+
+
